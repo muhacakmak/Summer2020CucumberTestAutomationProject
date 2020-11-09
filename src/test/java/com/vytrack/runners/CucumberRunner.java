6efac11f8c583b47d9b440b70287f78f49448762
Muha-B20 <muhammeddcakmak@outlook.com>
2020-11-08 22:20:27 -0700

    changed dryRun

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
+        dryRun = true,
         tags = "@negative_login",
         publish = true
 )
