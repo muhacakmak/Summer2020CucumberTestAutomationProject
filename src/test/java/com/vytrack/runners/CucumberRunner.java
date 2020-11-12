086662566e555d66541ad8591933dbe0df62a8c8
Muha-B20 <muhammeddcakmak@outlook.com>
2020-11-12 14:34:09 -0700

    muha

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@smoke",
+        tags = "@negative_login",
         publish = true
 
 )
